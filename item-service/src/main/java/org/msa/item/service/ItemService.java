ec3cb3638dd2466d8f92b98dc85de73bf9f7f769
doyoun <doyoun8813@gmail.com>
2023-12-05 19:36:38 +0900

    mariaDB dependency 설정 jpa dependency 설정 item insert 로직 추가

---
+++ item-service/src/main/java/org/msa/item/service/ItemService.java
@@ -0,0 +1,35 @@
+package org.msa.item.service;
+
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import org.msa.item.domain.Item;
+import org.msa.item.dto.ItemDTO;
+import org.msa.item.repository.ItemRepository;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class ItemService {
+
+    private final ItemRepository itemRepository;
+
+    public void insertItem(ItemDTO itemDTO){
+        String date = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMddHHmmss"));
+
+        Item item = Item.builder()
+            .id(itemDTO.getId())
+            .name(itemDTO.getName())
+            .description(itemDTO.getDescription())
+            .count(itemDTO.getCount())
+            .regDts(date)
+            .updDts(date)
+            .build();
+
+        itemRepository.save(item);
+    }
+}
